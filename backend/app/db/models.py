bca559660380da3b3f9650e70a344157414c402e
javierrc8 <jarocorodriguez@gmail.com>
2025-04-29 11:17:03 +0200

    feat: add Prediction table

--- backend/app/db/models.py
+++ backend/app/db/models.py
@@ -1,8 +1,18 @@
 from typing import List
-from sqlalchemy import ForeignKey, String
+from sqlalchemy import ForeignKey, String, Enum, DateTime, func, Float
+import enum
 from sqlalchemy.orm import declarative_base, Mapped, mapped_column, relationship
 
 Base = declarative_base()
+
+class FoulPrediction(enum.Enum):
+    NO_FOUL = 0
+    FOUL = 1
+
+class SeverityPrediction(enum.Enum):
+    NO_CARD = 0
+    RED_CARD = 1
+    YELLOW_CARD = 2
 
 class User(Base):
     __tablename__ = "user_account"
@@ -21,6 +31,9 @@ class Action(Base):
     id: Mapped[int] = mapped_column(primary_key=True)
     user_id: Mapped[int] = mapped_column(ForeignKey("user_account.id"))
     user: Mapped["User"] = relationship(back_populates="actions")
+    created_at: Mapped[DateTime] = mapped_column(
+        DateTime(timezone=True), server_default=func.now(), nullable=False
+    )
     clips: Mapped[List["Clip"]] = relationship(
         back_populates="action", cascade="all, delete-orphan"
     )
@@ -37,3 +50,20 @@ class Clip(Base):
 
     def __repr__(self) -> str:
         return f"Clip(id={self.id!r}, filename={self.filename!r})"
+    
+class Prediction(Base):
+    __tablename__ = "prediction"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    action_id: Mapped[int] = mapped_column(ForeignKey("action.id"))
+    action: Mapped["Action"] = relationship()
+
+    is_foul: Mapped[FoulPrediction] = mapped_column(Enum(FoulPrediction), nullable=False)
+    foul_prediction: Mapped[float] = mapped_column(Float, nullable=False)
+    
+    severity: Mapped[SeverityPrediction] = mapped_column(Enum(SeverityPrediction), nullable=False)
+    no_card_confidence: Mapped[float] = mapped_column(Float, nullable=False)
+    red_card_confidence: Mapped[float] = mapped_column(Float, nullable=False)
+    yellow_card_confidence: Mapped[float] = mapped_column(Float, nullable=False)
+
+    def __repr__(self) -> str:
+        return f"Prediction(id={self.id!r}, action_id={self.action_id!r}, is_foul={self.is_foul!r}, severity={self.severity!r})"
